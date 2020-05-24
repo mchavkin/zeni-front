dd9e40d0333254701cbeae993b637bed3979aab3
mchavkin <mchavkin@gmail.com>
2020-05-24 11:43:10 +0300

    Scroller test unobserve

--- src/components/InfiniteScroller/InfiniteScroller.test.js
+++ src/components/InfiniteScroller/InfiniteScroller.test.js
@@ -13,19 +13,27 @@ it("should instantiate IntersectionObserver and connect observation to scrolling
         currentEntries: 10
     }
     const observe = jest.fn()
+    const unobserve = jest.fn()
 
 
-    window.IntersectionObserver = jest.fn(function (cb) {
+    window.IntersectionObserver = jest.fn(function () {
         this.observe = observe
+        this.unobserve = unobserve
     })
-    const {getByTestId} = render(<InfiniteScroller {...props}/>)
+    const {getByTestId, rerender} = render(<InfiniteScroller {...props}/>)
     expect(window.IntersectionObserver.mock.calls.length).toBe(1)
     expect(observe).toBeCalledWith(getByTestId("scroller"))
 
     const observerCallback = window.IntersectionObserver.mock.calls[0][0]
     observerCallback([{isIntersecting: true}])
-
     expect(callback).toBeCalled()
+
+    rerender(<InfiniteScroller maxEntries={100} currentEntries={10} fetchEntries={()=>{}}/>)
+    expect(observe).toHaveBeenCalledTimes(1)
+    expect(unobserve).not.toHaveBeenCalled()
+
+    rerender(<InfiniteScroller maxEntries={100} currentEntries={100} fetchEntries={()=>{}}/>)
+    expect(unobserve).toHaveBeenCalled()
 
 
 })
